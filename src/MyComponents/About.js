255c9102e95994501cc8f2cc4fc115f0c111caee
Pandeyaarushi <pandey.aarushi16@gmail.com>
2023-05-08 16:59:18 +0530

    initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about page
+            <p>This application allows you to create dynamic to-do pages. It helps us to add task in the list. User can delete tasks from the list displayed upon completion. </p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer className="p">
+            <p className="text">
+            Copyright @MyTaskList
+            </p>
+        </footer>
+    )
+}
